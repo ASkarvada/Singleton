f3f6f05dcb2cef9ca832f64d01e00775261596a1
Adam Škarvada <adam.skarvada@seznam.cz>
2021-01-26 19:12:15 +0100

    Přidejte soubory projektu.

---
+++ Singleton/Osoba.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    
+    public class Osoba
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string BirthNumber { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public Osoba(string name, string surname, string birthNumber, DateTime birthDate)
+        {
+            Name = name;
+            Surname = surname;
+            BirthNumber = birthNumber;
+            BirthDate = birthDate;
+        }
+
+        
+    }
+    
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public sealed class Singleton
+    {
+        public Dictionary<string, Osoba> people = new Dictionary<string, Osoba>();
+
+        private static Singleton _instance = null;
+        private static readonly object padlock = new object();
+
+        Singleton()
+        {
+            people.Add("010213/0812", new Osoba("Marek", "Jarouš", "010213/0812", new DateTime(2001, 2, 13)));
+            people.Add("010213/0812", new Osoba("Marek", "Jarouš", "010213/0812", new DateTime(2001, 2, 13)));
+            people.Add("010213/0812", new Osoba("Marek", "Jarouš", "010213/0812", new DateTime(2001, 2, 13)));
+            people.Add("010213/0812", new Osoba("Marek", "Jarouš", "010213/0812", new DateTime(2001, 2, 13)));
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Singleton();
+                    }
+                    return _instance;
+                }
+            }
+        }
+    }
+}
